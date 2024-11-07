fa72414d3ae9d705e9939a19dbd41c99877ae3ac
jst-r <jstr271@gmail.com>
2024-11-07 19:18:14 +0100

    token type

---
+++ compiler/token.go
@@ -0,0 +1,57 @@
+package compiler
+
+type Token struct {
+	Type    TokenType
+	Literal string
+	Line    int
+}
+
+type TokenType int
+
+const (
+	// Single-character tokens
+	TokenLeftParen TokenType = iota
+	TokenRightParen
+	TokenLeftBrace
+	TokenRightBrace
+	TokenComma
+	TokenDot
+	TokenMinus
+	TokenPlus
+	TokenSemicolon
+	TokenSlash
+	TokenStar
+	// One or two character tokens
+	TokenBang
+	TokenBangEqual
+	TokenEqual
+	TokenEqualEqual
+	TokenGreater
+	TokenGreaterEqual
+	TokenLess
+	TokenLessEqual
+	// Literals
+	TokenIdentifier
+	TokenString
+	TokenNumber
+	// Keywords
+	TokenAnd
+	TokenClass
+	TokenElse
+	TokenFalse
+	TokenFor
+	TokenFun
+	TokenIf
+	TokenNil
+	TokenOr
+	TokenPrint
+	TokenReturn
+	TokenSuper
+	TokenThis
+	TokenTrue
+	TokenVar
+	TokenWhile
+	// Special
+	TokenError
+	TokenEof
+)
