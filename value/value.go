ef6b5d91e9f4821cf8d3550cb5f32f83b8453b30
jst-r <jstr271@gmail.com>
2024-11-13 18:58:31 +0100

    move primitive values to a separate file

--- value/value.go
+++ value/value.go
@@ -21,29 +21,6 @@ type Value struct {
 
 type ValueArray []Value
 
-var Nil = Value{ValueTypeNil, 0}
-
-func Float(val float64) Value {
-	return Value{ValueTypeFloat, unsafeBitCast[float64, uint64](val)}
-}
-
-// Unsafe, caller must verify the type before calling this method
-func (v Value) AsFloat() float64 {
-	return unsafeBitCast[uint64, float64](v.mem)
-}
-
-func Bool(val bool) Value {
-	if val {
-		return Value{ValueTypeBool, 1}
-	} else {
-		return Value{ValueTypeBool, 0}
-	}
-}
-
-func (v Value) AsBool() bool {
-	return v.mem != 0
-}
-
 func (v Value) IsFalsey() bool {
 	return v.IsNil() || (v.IsBool() && !v.AsBool())
 }
@@ -87,22 +64,6 @@ func (v Value) FormatString() string {
 	default:
 		panic("unreachable")
 	}
-}
-
-func (v Value) Is(t ValueType) bool {
-	return v.t == t
-}
-
-func (v Value) IsNil() bool {
-	return v.t == ValueTypeNil
-}
-
-func (v Value) IsBool() bool {
-	return v.t == ValueTypeBool
-}
-
-func (v Value) IsFloat() bool {
-	return v.t == ValueTypeFloat
 }
 
 func unsafeBitCast[A, B any](a A) B {
