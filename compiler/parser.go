3513dd2666203651af7d006e4ec515ffe29f0562
jst-r <jstr271@gmail.com>
2024-11-19 12:58:00 +0100

    move grammar to a separate file

--- compiler/parser.go
+++ compiler/parser.go
@@ -45,72 +45,12 @@ func NewParser(input string) *Parser {
 	return p
 }
 
-func (p *Parser) declaration() {
-	if p.match(TokenVar) {
-		p.varDeclaration()
-	} else {
-		p.statement()
-	}
-
-	if p.panicMode {
-		p.syncronize()
-	}
-}
-
-func (p *Parser) varDeclaration() {
-	global := p.parseVariable("Expected variable name")
-
-	if p.match(TokenEqual) {
-		p.expression()
-	} else {
-		p.emitByte(bytecode.OpNil) // var x; => var x = nil
-	}
-
-	p.consume(TokenSemicolon, "Expected ; after variable declaration")
-	p.defineVariable(global)
-}
-
 func (p *Parser) defineVariable(global uint8) {
 	if p.compiler.scopeDepth > 0 {
 		return
 	}
 
 	p.emitBytes(bytecode.OpDefineGlobal, global)
-}
-
-func (p *Parser) statement() {
-	if p.match(TokenPrint) {
-		p.printStatement()
-	} else if p.match(TokenRightBrace) {
-		p.compiler.beginScope()
-		p.block()
-		p.compiler.endScope()
-	} else {
-		p.expressionStatement()
-	}
-}
-
-func (p *Parser) block() {
-	for !p.check(TokenRightBrace) && !p.check(TokenEof) {
-		p.statement()
-	}
-	p.consume(TokenRightBrace, "Expected } after block")
-}
-
-func (p *Parser) printStatement() {
-	p.expression()
-	p.consume(TokenSemicolon, "Expected ; after print statement")
-	p.emitByte(bytecode.OpPrint)
-}
-
-func (p *Parser) expressionStatement() {
-	p.expression()
-	p.consume(TokenSemicolon, "Expected ; after expression")
-	p.emitByte(bytecode.OpPop)
-}
-
-func (p *Parser) expression() {
-	p.parsePrecedence(PrecedenceAssignment)
 }
 
 func (p *Parser) parseVariable(errorMessage string) uint8 {
