365bcb8bf1d34ecd3f7a8c6c2f764c20e6c6d8de
jst-r <jstr271@gmail.com>
2024-11-19 15:57:33 +0100

    tests for if

---
+++ test/control-flow_test.go
@@ -0,0 +1,22 @@
+package test
+
+import "testing"
+
+func TestControlFlow(t *testing.T) {
+	var cases = []Case{
+		NewCase("If", `
+	var x = 1;
+	if (x == 1) {
+		print "x is 1";
+	}`).ExpectLines("x is 1"),
+
+		NewCase("If else", `
+	var x = 1;
+	if (x == 1) {
+		print "x is 1";
+	} else {
+		print "x is not 1";
+	}`).ExpectLines("x is not 1"),
+	}
+	RunCases(cases, t)
+}
--- test/variable_test.go
+++ test/variable_test.go
@@ -4,48 +4,47 @@ import (
 	"testing"
 )
 
-var cases = []Case{
-	NewCase("Globals", `
-	var x = 1;
-	print x;
-	x = x + 1;
-	print x;
-	`).ExpectLines("1", "2"),
-	NewCase("Locals", `
-	{
-		var x = 1;
-		var y = 2;
-		print x;
-		print y;
-		y = 3;
-		print y;
-		print x + y;
-	}
-	`).ExpectLines("1", "2", "3", "4"),
-	NewCase("Local shadowing", `
-	var x = 1;
-	{
-		var x = 2;
-		{
-			var x = 3;
-			print x;
-		}
-		print x;
-	}
-	print x;
-	`).ExpectLines("3", "2", "1"),
-	NewCase("Shadow with use", `
-	{
-		var x = 1;
-		{
-			var x = x + 1;
-			print x;
-		}
-		print x;
-	}
-	`).ExpectCompileErrors("[line 5]error at x: can not read local variable in its own initializer"),
-}
-
 func TestVariables(t *testing.T) {
+	var cases = []Case{
+		NewCase("Globals", `
+		var x = 1;
+		print x;
+		x = x + 1;
+		print x;
+		`).ExpectLines("1", "2"),
+		NewCase("Locals", `
+		{
+			var x = 1;
+			var y = 2;
+			print x;
+			print y;
+			y = 3;
+			print y;
+			print x + y;
+		}
+		`).ExpectLines("1", "2", "3", "4"),
+		NewCase("Local shadowing", `
+		var x = 1;
+		{
+			var x = 2;
+			{
+				var x = 3;
+				print x;
+			}
+			print x;
+		}
+		print x;
+		`).ExpectLines("3", "2", "1"),
+		NewCase("Shadow with use", `
+		{
+			var x = 1;
+			{
+				var x = x + 1;
+				print x;
+			}
+			print x;
+		}
+		`).ExpectCompileErrors("[line 5]error at x: can not read local variable in its own initializer"),
+	}
 	RunCases(cases, t)
 }
